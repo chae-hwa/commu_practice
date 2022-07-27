6e84fc685da104c2a7c569746cd8f06ad29d1418
chae-hwa <ihj0519@naver.com>
2022-07-27 17:03:06 +0900

    POST 방식으로 입력된 게시물 작성 데이터 확인

--- src/main/java/com/ll/exam/DispatchServlet.java
+++ src/main/java/com/ll/exam/DispatchServlet.java
@@ -12,23 +12,38 @@ import java.io.IOException;
 
 @WebServlet("/usr/*")
 public class DispatchServlet extends HttpServlet {
+
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
         Rq rq = new Rq(req, resp);
         MemberController memberController = new MemberController();
         ArticleController articleController = new ArticleController();
 
-        String url = req.getRequestURI();
 
-        switch(url){
-            case "/usr/article/list/free":
-                articleController.showList(rq);
+        switch(rq.getMethod()){
+            case "GET":
+                switch (rq.getPath()){
+                    case "/usr/article/list/free":
+                        articleController.showList(rq);
+                        break;
+                    case "/usr/article/write/free":
+                        articleController.showWrite(rq);
+                        break;
+                    case "/usr/member/login":
+                        memberController.showLogin(rq);
+                }
                 break;
-            case "/usr/article/write/free":
-                articleController.showWrite(rq);
+            case "POST":
+                switch(rq.getPath()){
+                    case "/usr/article/write/free":
+                        articleController.doWrite(rq);
+                        break;
+                }
                 break;
-            case "/usr/member/login":
-                memberController.showLogin(rq);
         }
+    }
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) {
+        doGet(req, resp);
     }
 }
