5ed06d53d86f1d6b593f500b2dd37e18b9e726e5
chae-hwa <ihj0519@naver.com>
2022-07-28 22:08:38 +0900

    게시물 수정폼

--- src/main/java/com/ll/exam/DispatchServlet.java
+++ src/main/java/com/ll/exam/DispatchServlet.java
@@ -31,6 +31,9 @@ public class DispatchServlet extends HttpServlet {
                     case "/usr/article/delete":
                         articleController.doDelete(rq);
                         break;
+                    case "/usr/article/modify":
+                        articleController.showModify(rq);
+                        break;
                     case "/usr/member/login":
                         memberController.showLogin(rq);
                         break;
@@ -40,6 +43,9 @@ public class DispatchServlet extends HttpServlet {
                 switch (rq.getActionPath()) {
                     case "/usr/article/write":
                         articleController.doWrite(rq);
+                        break;
+                    case "/usr/article/modify":
+                        articleController.doModify(rq);
                         break;
                 }
                 break;
