51549bcf49fbe20497a017577c52441e047b29f2
chae-hwa <ihj0519@naver.com>
2022-07-28 22:25:50 +0900

    ArticleDto에 @Data로 교체

--- src/main/java/com/ll/exam/article/dto/ArticleDto.java
+++ src/main/java/com/ll/exam/article/dto/ArticleDto.java
@@ -1,34 +1,16 @@
 package com.ll.exam.article.dto;
 
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 
-@ToString
+
+@Data
+@AllArgsConstructor
 public class ArticleDto {
-    @Getter
-    @Setter
+
     private long id;
-    @Getter
-    @Setter
+
     private String title;
-    @Getter
-    @Setter
+
     private String body;
-
-    @Override
-    public String toString() {
-        return "ArticleDto{" +
-                "id=" + id +
-                ", title='" + title + '\'' +
-                ", body='" + body + '\'' +
-                '}';
-    }
-
-    public ArticleDto(long id, String title, String body) {
-        this.id = id;
-        this.title = title;
-        this.body = body;
-    }
 
 }
